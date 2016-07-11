6a60d9e1ca23f1798b8fca08798dc1dfc2e23607
Cristina <cristina_roman@dennis.co.uk>
2016-07-11 17:51:26 +0100

    testing admin form

--- src/Plugin/Block/ScriptBlock.php
+++ src/Plugin/Block/ScriptBlock.php
@@ -14,14 +14,40 @@ use Drupal\Core\Block\BlockBase;
  */
 class ScriptBlock extends BlockBase {
 
+ /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['label_display' => FALSE];
+  }
+
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-    $build['script_block']['#markup'] = 'Implement ScriptBlock.';
+    // $build = [];
 
-    return $build;
+    // $build['script_block']['#markup'] = 'Implement ScriptBlock.';
+    // // $build['script_block']['#markup'] = '\Drupal\block_script\Controller\ScriptBlockController::content()';
+    // // $build['script_block']['#markup'] = $content;
+    // // $build['script_block']['#markup'] = '<script></script>';
+    // return $build;
+//     $config = $this->getConfiguration();
+ $config = \Drupal::config('block_script.settings');
+
+
+	$output = array();
+    //Add this two linew if you want to create a dynamic contents block
+    $output['cache']['#disabled'] = TRUE;
+    $output['cache']['max_age']['#value'] = 0;
+
+    $html = "";
+    $html .= 'This is a programmatically created block  :  ' . date("H:i:s") ;
+    $output[] = [      '#markup' => $html,    ];
+    return $output;
+
+
+
   }
 
 }
