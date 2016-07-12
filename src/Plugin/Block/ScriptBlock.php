a4a6a3d879057ba0aeea53db8b4f69af7d5db59b
Cristina <cristina_roman@dennis.co.uk>
2016-07-12 15:37:05 +0100

    working!

--- src/Plugin/Block/ScriptBlock.php
+++ src/Plugin/Block/ScriptBlock.php
@@ -24,30 +24,31 @@ class ScriptBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
-  public function build() {
-    // $build = [];
+  	public function build() {
 
-    // $build['script_block']['#markup'] = 'Implement ScriptBlock.';
-    // // $build['script_block']['#markup'] = '\Drupal\block_script\Controller\ScriptBlockController::content()';
-    // // $build['script_block']['#markup'] = $content;
-    // // $build['script_block']['#markup'] = '<script></script>';
-    // return $build;
-//     $config = $this->getConfiguration();
- $config = \Drupal::config('block_script.settings');
+  	// Get info from admin form settings.
+	$config = \Drupal::config('block_script.ScriptConfig');
+
+    $enabled = $config->get('doubleclick_floodlight_enabled');
+  	$region = $config->get('doubleclick_floodlight_region');
+  	$account_id = $config->get('doubleclick_floodlight_account_id');
+  	$type = $config->get('doubleclick_floodlight_type');
+  	$cat = $config->get('doubleclick_floodlight_cat');
+  	$show_standard = $config->get('doubleclick_floodlight_show_standard');
+  	$show_unique = $config->get('doubleclick_floodlight_show_unique');
+
+   return array(
+      '#theme' => 'script_block',
+      '#test_var' => $this->t('Test Value'),
+      '#account_id' => $account_id,
+      '#type' => $type,
+      '#cat' => $cat,
+      '#show_standard' => $show_standard,
+      '#show_unique' => $show_unique,
+      '#enabled' => $enabled,
+   );
 
 
-	$output = array();
-    //Add this two linew if you want to create a dynamic contents block
-    $output['cache']['#disabled'] = TRUE;
-    $output['cache']['max_age']['#value'] = 0;
-
-    $html = "";
-    $html .= 'This is a programmatically created block  :  ' . date("H:i:s") ;
-    $output[] = [      '#markup' => $html,    ];
-    return $output;
-
-
-
-  }
+	}
 
 }
